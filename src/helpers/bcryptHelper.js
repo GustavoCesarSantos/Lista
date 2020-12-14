8433240d31a1cd08112432115f4790d715c047bc
GustavoCesarSantos <gustavocs789@gmail.com>
2020-12-13 23:41:00 -0300

    Melhorado semantica da funcao encryptPassword e adicionado funcao comparePassword

--- src/helpers/bcryptHelper.js
+++ src/helpers/bcryptHelper.js
@@ -3,7 +3,11 @@ const bcrypt = require('bcrypt')
 module.exports = {
   async encryptPassword (password) {
     const salt = await bcrypt.genSalt(12)
-    const encryptPassword = await bcrypt.hash(password, salt)
-    return encryptPassword
+    const encryptedPassword = await bcrypt.hash(password, salt)
+    return encryptedPassword
+  },
+
+  async comparePassword (password, encryptedPassword) {
+    return await bcrypt.compare(password, encryptedPassword)
   }
 }
