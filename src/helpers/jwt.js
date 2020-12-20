bf58233fd14d89eb9d74ddaabec35f0726d7895f
GustavoCesarSantos <gustavocs789@gmail.com>
2020-12-19 23:49:27 -0300

    Criado helper jwt e funcao para gerar token jwt

---
+++ src/helpers/jwt.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = {
+  createToken (user) {
+    const payload = {
+      id: user.id
+    }
+
+    const token = jwt.sign(payload, process.env.KEY_JWT)
+    return token
+  }
+}
