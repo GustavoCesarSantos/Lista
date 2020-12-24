ffff09134c78e01eebbef1da3458ac57e00a195f
GustavoCesarSantos <gustavocs789@gmail.com>
2020-12-24 00:34:34 -0300

    Chamando config da blocklist no arquivo de server

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ require('dotenv').config()
 
 const app = require('./src/config/server/express')
 require('./src/config/databases/mysql')
+require('./src/config/databases/blocklist')
 
 app.listen(3000, () => console.info('Server on in port 3000'))
