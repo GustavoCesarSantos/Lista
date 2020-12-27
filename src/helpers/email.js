d24eb419c59cea2864ef457fe1fd413cc16f712b
GustavoCesarSantos <gustavocs789@gmail.com>
2020-12-26 23:13:50 -0300

    Refatorado helper de email

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -1,20 +1,29 @@
 const nodemailer = require('nodemailer')
 
-module.exports = {
-  async sendEmail (user) {
+class Email {
+  async sendEmail () {
     const testEmail = await nodemailer.createTestAccount()
     const transporter = nodemailer.createTransport({
       host: 'smtp.ethereal.email',
       auth: testEmail
     })
 
-    const sendedEmail = await transporter.sendMail({
-      from: '"Teste" <gustavocs789@gmail.com>',
-      to: user.email,
-      subject: 'teste de email',
-      text: 'TESTE TESTE TESTE'
-    })
+    const sendedEmail = await transporter.sendMail(this)
 
     console.log('URL: ' + nodemailer.getTestMessageUrl(sendedEmail))
   }
 }
+
+class VerificationEmail extends Email {
+  constructor (user) {
+    super()
+    this.address = `${process.env.BASE_URL}/users/${user.id}/email/verify`
+    this.from = '"Teste" <gustavocs789@gmail.com>'
+    this.to = user.email
+    this.subject = 'teste de email'
+    this.text = `Verifique seu e-mail aqui: ${this.address}`
+    this.html = `Verifique seu e-mail aqui: <a href="${this.address}">${this.address}</a>`
+  }
+}
+
+module.exports = { VerificationEmail }
