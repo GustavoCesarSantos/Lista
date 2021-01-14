8d546199aa877d77bb8c6fec42a02bd9a1df591f
GustavoCesarSantos <gustavocs789@gmail.com>
2021-01-14 19:42:56 -0300

    finalizado request model do componente anotacoes

---
+++ src/components/Annotation/requestsModel/AnnotationModel.js
@@ -0,0 +1,39 @@
+const Joi = require('joi')
+
+const schema = Joi.object({
+  id: [Joi.string(), Joi.number()],
+  listId: [Joi.string(), Joi.number()],
+  data: Joi.string().max(30)
+})
+
+class Annotation {
+  constructor (annotation) {
+    this.id = annotation.annotationId
+    this.listId = annotation.listId
+    this.data = annotation.data
+  }
+
+  async isValid () {
+    try {
+      await schema.validateAsync(this)
+    } catch (err) {
+      throw new Error(err.message)
+    }
+  }
+
+  async returnsAValidQuery () {
+    try {
+      await this.isValid()
+
+      const query = {}
+      for (const prop in this) {
+        if (typeof this[prop] !== 'undefined') query[prop] = this[prop]
+      }
+      return query
+    } catch (err) {
+      throw new Error(err.message)
+    }
+  }
+}
+
+module.exports = Annotation
