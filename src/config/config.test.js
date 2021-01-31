52377b28f5ca2cc784f49f27efdf4f3e158b4046
GustavoCesarSantos <gustavocs789@gmail.com>
2021-01-31 14:41:40 -0300

    Refazendo configs

---
+++ src/config/config.test.js
@@ -0,0 +1,10 @@
+const config = module.exports = {}
+
+// Config database Mysql
+config.DATABASE = 'list-test'
+config.USER = 'root'
+config.PASSWORD = '123456'
+config.HOST = 'localhost'
+
+// Token secret
+config.TOKEN_SECRET = 'HoMeMaRaNhA_2019_'
