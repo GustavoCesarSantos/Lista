fed805853ac06e06e4e32b4e60ca4269d2148ba6
GustavoCesarSantos <gustavocs789@gmail.com>
2021-01-31 22:57:49 -0300

    Tentando configurar jest

--- src/config/config.test.js
+++ src/config/config.test.js
@@ -5,6 +5,7 @@ config.DATABASE = 'list-test'
 config.USER = 'root'
 config.PASSWORD = '123456'
 config.HOST = 'localhost'
+config.DIALECT = 'sqlite'
 
 // Token secret
 config.TOKEN_SECRET = 'HoMeMaRaNhA_2019_'
