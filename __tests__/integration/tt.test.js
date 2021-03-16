a4326609460dd4e2dc0de373c50eda8f7afbcd4f
GustavoCesarSantos <gustavocs789@gmail.com>
2021-03-16 02:07:10 -0300

    Refatorado teste inicial

--- __tests__/integration/tt.test.js
+++ __tests__/integration/tt.test.js
@@ -1,5 +1,22 @@
+const request = require('supertest')
+const app = require('../../src/config/server/express')
+
+const UserDao = require('../../src/components/User/UserDao')
+
 describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true)
+  it('should test that true === true', async () => {
+    await UserDao.setUser({
+      email: 'test@test.com',
+      password: '12345678'
+    })
+
+    const response = await request(app)
+      .post('/login')
+      .send({
+        email: 'test@test.com',
+        password: '12345678'
+      })
+
+    expect(response.status).toBe(200)
   })
 })
