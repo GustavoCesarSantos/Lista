1df012f25bb8b79c5d5a9cf45f0c5a31372289fd
GustavoCesarSantos <gustavocs789@gmail.com>
2021-03-16 19:38:42 -0300

    Retrieved call to mysql file from server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 const app = require('./src/config/server/express')
-require('./src/config/databases/mysql')
 require('./src/config/databases/allowlist-refresh-token')
 require('./src/config/databases/blocklist-access-token')
 
