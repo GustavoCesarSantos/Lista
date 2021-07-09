68f721ebc739a7a17be13f366de4c6e3edad80d0
GustavoCesarSantos <gustavocs789@gmail.com>
2021-07-09 18:23:27 -0300

    Added a test in CreateUser unit test

--- __tests__/units/User/CreateUser.test.js
+++ __tests__/units/User/CreateUser.test.js
@@ -1,17 +1,23 @@
 const truncate = require('../../utils/truncate')
 const UserDao = require('../../../src/components/User/UserDao')
 
-describe('Create user unit test', () => {
+describe('Create user', () => {
   beforeEach(async () => await truncate())
 
   afterAll(async () => await truncate())
 
-  it('Should persist in base a valid user when pass a valid email and password', async () => {
-    const mockUser = {
-      email: 'testUnit1@test.com',
-      password: '12345678'
-    }
+  const mockUser = {
+    email: 'testUnit1@test.com',
+    password: '12345678'
+  }
 
+  it('Should not persist in base a user when this user already exists', async () => {
+    await UserDao.setUser(mockUser)
+
+    await expect(UserDao.setUser(mockUser)).rejects.toThrow()
+  })
+
+  it('Should persist in base a valid user when pass a valid email and password', async () => {
     await UserDao.setUser(mockUser)
 
     const [{ dataValues }] = await UserDao.getUsers({ email: mockUser.email })
