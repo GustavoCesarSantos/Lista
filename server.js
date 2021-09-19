312f8619853886b7c57dc84d8987745ea1997249
GustavoCesarSantos <gustavocs789@gmail.com>
2021-09-19 00:42:57 -0300

    Added log for server on and added .log in gitignore

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
+const logger = require('./src/helpers/logger')
+
 const app = require('./src/config/server/express')
 require('./src/config/databases/allowlist-refresh-token')
 require('./src/config/databases/blocklist-access-token')
 
-app.listen(3000, () => console.info('Server on in port 3000'))
+app.listen(3000, logger.info('Server on in port 3000'))
