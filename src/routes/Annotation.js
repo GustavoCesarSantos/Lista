419bce2add07d29e6aa99d0feae37a1a879b6f46
GustavoCesarSantos <gustavocs789@gmail.com>
2021-10-15 19:09:43 -0300

    fix: Changed old create annotation method for the newest

--- src/routes/Annotation.js
+++ src/routes/Annotation.js
@@ -1,12 +1,16 @@
 const authenticationToken = require('../middlewares/authenticationToken')
 const AnnotationController = require('../components/Annotation/AnnotationController')
 
+const CreateAnnotationController = require('../components/Annotation/CreateAnnotation/CreateAnnotationController')
+
+const createAnnotationController = new CreateAnnotationController()
+
 module.exports = (app) => {
   app.route('/annotations')
     .get(authenticationToken.bearer, AnnotationController.getAnnotations)
 
   app.route('/lists/:listId/annotations')
-    .post(authenticationToken.bearer, AnnotationController.setAnnotation)
+    .post(authenticationToken.bearer, createAnnotationController.handler)
 
   app.route('/annotations/:annotationId')
     .get(authenticationToken.bearer, AnnotationController.getAnnotation)
