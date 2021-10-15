413f8f29050896562a319837888b5f6d165cdbbb
GustavoCesarSantos <gustavocs789@gmail.com>
2021-10-15 19:07:39 -0300

    feat: Added logs for create annotation controller layer

--- src/components/Annotation/CreateAnnotation/CreateAnnotationController.js
+++ src/components/Annotation/CreateAnnotation/CreateAnnotationController.js
@@ -1,17 +1,22 @@
 const CreateAnnotationRequestDTO = require('./CreateAnnotationRequestDTO')
 const CreateAnnotationService = require('./CreateAnnotationService')
+const logger = require('../../../helpers/logger')
 
 class CreateAnnotationController {
-  static async handler (request, response) {
+  async handler (request, response) {
     try {
+      logger.info('Usuário está tentando cadastrar uma anotação.')
       const createAnnotationRequestDTO = new CreateAnnotationRequestDTO({
         ...request.params,
         ...request.body
       })
       const createAnnotationService = new CreateAnnotationService()
       await createAnnotationService.execute(createAnnotationRequestDTO)
+      logger.info('Anotação cadastrada com sucesso.')
       response.status(201).end()
     } catch (err) {
+      if (!err.httpCode) err.httpCode = 500
+      logger.error(`http status code: ${err.httpCode} - ${err.message}`)
       response.status(err.httpCode).send(err.message)
     }
   }
