8d4425d12613cc4556a12c801ad6f141af71d2d7
GustavoCesarSantos <gustavocs789@gmail.com>
2021-10-31 15:24:28 -0300

    fix(Annotation): Refactored routes file to conform to create and list annotation

--- src/routes/Annotation.js
+++ src/routes/Annotation.js
@@ -2,18 +2,24 @@ const authenticationToken = require('../middlewares/authenticationToken')
 const AnnotationController = require('../components/Annotation/AnnotationController')
 
 const CreateAnnotationController = require('../components/Annotation/CreateAnnotation/CreateAnnotationController')
+const CreateAnnotationRepository = require('../components/Annotation/CreateAnnotation/CreateAnnotationRepository')
+const ListAnnotationController = require('../components/Annotation/ListAnnotation/ListAnnotationController')
+const ListAnnotationRepository = require('../components/Annotation/ListAnnotation/ListAnnotationRepository')
 
-const createAnnotationController = new CreateAnnotationController()
+const createAnnotationRepository = new CreateAnnotationRepository()
+const createAnnotationController = new CreateAnnotationController(createAnnotationRepository)
+const listAnnotationRepository = new ListAnnotationRepository()
+const listAnnotationController = new ListAnnotationController(listAnnotationRepository)
 
 module.exports = (app) => {
   app.route('/annotations')
     .get(authenticationToken.bearer, AnnotationController.getAnnotations)
 
   app.route('/lists/:listId/annotations')
-    .post(authenticationToken.bearer, createAnnotationController.handler)
+    .post(authenticationToken.bearer, createAnnotationController.handler.bind(createAnnotationController))
 
   app.route('/annotations/:annotationId')
-    .get(authenticationToken.bearer, AnnotationController.getAnnotation)
+    .get(authenticationToken.bearer, listAnnotationController.handler.bind(listAnnotationController))
     .put(authenticationToken.bearer, AnnotationController.updateAnnotation)
     .delete(authenticationToken.bearer, AnnotationController.deleteAnnotation)
 }
