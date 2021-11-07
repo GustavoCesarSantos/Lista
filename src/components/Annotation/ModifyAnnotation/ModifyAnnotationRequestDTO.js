c78973a6d9aa4d4039e5f9f8f389b0a662b2d229
GustavoCesarSantos <gustavocs789@gmail.com>
2021-11-06 23:54:17 -0300

    feat(annotation): Created modify annotation

---
+++ src/components/Annotation/ModifyAnnotation/ModifyAnnotationRequestDTO.js
@@ -0,0 +1,9 @@
+class ModifyAnnotationRequestDTO {
+  constructor (annotation) {
+    this.annotationId = annotation.annotationId
+    this.listId = annotation.listId
+    this.contents = annotation.contents
+  }
+}
+
+module.exports = ModifyAnnotationRequestDTO
---
+++ src/components/Annotation/ModifyAnnotation/ModifyAnnotationService.js
@@ -0,0 +1,18 @@
+const Annotation = require('../requestsModel/AnnotationModel')
+const ErrorHandler = require('../../../helpers/ErrorHandler')
+
+class ModifyAnnotationService {
+  constructor (annotationRepository) {
+    this.annotationRepository = annotationRepository
+  }
+
+  async execute (modifyAnnotationRequestDTO) {
+    const annotation = new Annotation(modifyAnnotationRequestDTO)
+    await annotation.isValid()
+    const hasAnnotation = await this.annotationRepository.findOne(annotation.id)
+    if (!hasAnnotation) throw new ErrorHandler('Anotação não encontrada', 404)
+    await this.annotationRepository.modify(annotation)
+  }
+}
+
+module.exports = ModifyAnnotationService
