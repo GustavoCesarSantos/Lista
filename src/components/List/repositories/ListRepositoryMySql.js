1100c0ea8014265e0bac250c29a03f4cc93c6c0d
GustavoCesarSantos <gustavocs789@gmail.com>
2021-11-07 15:20:33 -0300

    feat(list): Created list repository

---
+++ src/components/List/repositories/ListRepositoryMySql.js
@@ -0,0 +1,30 @@
+const IListRepository = require('./IListRepository')
+
+class ListRepositoryMySql extends IListRepository {
+  constructor (listModel) {
+    super()
+    this.listModel = listModel
+  }
+
+  async create (list) {
+    await this.listModel.create(list)
+  }
+
+  async findOne (listId) {
+    return await this.listModel.findByPk(listId, { include: { association: 'Annotations' } })
+  }
+
+  async findMany (query) {
+    return await this.listModel.findAll({ where: query })
+  }
+
+  async modify (list) {
+    await this.listModel.update(list, { where: { id: list.id } })
+  }
+
+  async remove (listId) {
+    await this.listModel.destroy({ where: { id: listId } })
+  }
+}
+
+module.exports = ListRepositoryMySql
