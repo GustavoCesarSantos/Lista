f19f705c155bfb5805e5b2d439d7809a942564d6
GustavoCesarSantos <gustavocs789@gmail.com>
2021-11-14 20:03:11 -0300

    feat(user): Created user repository

---
+++ src/components/User/Repositories/IUserRepository.js
@@ -0,0 +1,9 @@
+class IUserRepository {
+  create (user) { throw new Error('Método não implementado.') }
+  findOne (userId) { throw new Error('Método não implementado.') }
+  findMany (query) { throw new Error('Método não implementado.') }
+  modify (user) { throw new Error('Método não implementado.') }
+  remove (userId) { throw new Error('Método não implementado.') }
+}
+
+module.exports = IUserRepository
