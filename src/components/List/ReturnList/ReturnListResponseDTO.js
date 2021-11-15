d7861ede10dcefa9f97df44875a1472eccdf01d3
GustavoCesarSantos <gustavocs789@gmail.com>
2021-11-14 23:36:30 -0300

    fix(list): Fixed response dto

--- src/components/List/ReturnList/ReturnListResponseDTO.js
+++ src/components/List/ReturnList/ReturnListResponseDTO.js
@@ -1,8 +1,8 @@
 class ReturnListResponseDTO {
   constructor (list) {
     this.id = list.id
-    this.contents = list.contents
-    this.annotations = list.annotations
+    this.name = list.name
+    this.annotations = list.Annotations
   }
 }
 
