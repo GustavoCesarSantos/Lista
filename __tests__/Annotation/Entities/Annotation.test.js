18ade325ba6230ec5c6053531f543ff91f1680c7
GustavoCesarSantos <gustavocs789@gmail.com>
2021-11-15 21:29:27 -0300

    feat(annotation): Create entity annotation test

---
+++ __tests__/Annotation/Entities/Annotation.test.js
@@ -0,0 +1,34 @@
+const Annotation = require('../../../src/components/Annotation/Entities/Annotation')
+
+describe('ENTITY ANNOTATION TEST', () => {
+  test('Should not create a annotation object when passed a integer value in contents property', () => {
+    const data = {
+      listId: 1,
+      contents: 1
+    }
+    const annotation = new Annotation(data)
+    annotation.isValid().catch(error => expect(error.message).toBe('"contents" must be a string'))
+  })
+
+  test('Should not create a query', () => {
+    const data = {}
+    const annotation = new Annotation(data)
+    expect(annotation.returnsAValidQuery()).resolves.toEqual({})
+  })
+
+  test('Should create a valid annotation object', () => {
+    const data = {
+      annotationId: 1,
+      listId: 1,
+      contents: 'asdf'
+    }
+    const annotation = new Annotation(data)
+    expect(annotation).toEqual({ id: 1, listId: 1, contents: 'asdf' })
+  })
+
+  test('Should create a valid query', () => {
+    const data = { contents: 'teste' }
+    const annotation = new Annotation(data)
+    expect(annotation.returnsAValidQuery()).resolves.toEqual(data)
+  })
+})
