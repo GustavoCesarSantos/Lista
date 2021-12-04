76daa2e5e4b4bf84bf80a0a12c12226d25b5ff73
Gustavo Cesar <gustavocs789@gmail.com>
2021-12-04 18:48:04 -0300

    feat: Created status route

--- src/config/server/express.js
+++ src/config/server/express.js
@@ -5,14 +5,15 @@ const swaggerUi = require('swagger-ui-express')
 const routesUser = require('../../routes/User')
 const routesList = require('../../routes/List')
 const routesAnnotation = require('../../routes/Annotation')
-// const swaggerFile = require('../doc/swagger_output.json')
+const swaggerFile = require('../doc/swagger_output.json')
 
 const app = express()
 app.use(express.json())
 
 app.use(helmet())
 
-// app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.get('/status', (request, response) => response.status(200).json({ message: 'ok' }))
 
 // Import strategys
 require('../../strategys/verifyUser')
