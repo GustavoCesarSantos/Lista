ac29fbb7ca444834eaebbb4a50f43e1153fc60a4
Gustavo Cesar <gustavocs789@gmail.com>
2021-12-04 18:47:42 -0300

    refactor: Refactored routes from components

--- src/routes/Annotation.js
+++ src/routes/Annotation.js
@@ -1,18 +1,34 @@
 const { Annotations } = require('../database/models')
-const AnnotationRepositoryMySql = require('../components/Annotation/Repositories/AnnotationRepositoryMySql')
+
 const authenticationToken = require('../middlewares/authenticationToken')
+
+const AnnotationRepositoryMySql = require('../components/Annotation/Repositories/AnnotationRepositoryMySql')
+
 const CreateAnnotationController = require('../components/Annotation/CreateAnnotation/CreateAnnotationController')
 const ListAnnotationController = require('../components/Annotation/ListAnnotation/ListAnnotationController')
 const ListAnnotationsController = require('../components/Annotation/ListAnnotations/ListAnnotationsController')
 const ModifyAnnotationController = require('../components/Annotation/ModifyAnnotation/ModifyAnnotationController')
 const RemoveAnnotationController = require('../components/Annotation/RemoveAnnotation/RemoveAnnotationController')
 
+const CreateAnnotationService = require('../components/Annotation/CreateAnnotation/CreateAnnotationService')
+const ListAnnotationService = require('../components/Annotation/ListAnnotation/ListAnnotationService')
+const ListAnnotationsService = require('../components/Annotation/ListAnnotations/ListAnnotationsService')
+const ModifyAnnotationService = require('../components/Annotation/ModifyAnnotation/ModifyAnnotationService')
+const RemoveAnnotationService = require('../components/Annotation/RemoveAnnotation/RemoveAnnotationService')
+
 const annotationRepositoryMySql = new AnnotationRepositoryMySql(Annotations)
-const createAnnotationController = new CreateAnnotationController(annotationRepositoryMySql)
-const listAnnotationController = new ListAnnotationController(annotationRepositoryMySql)
-const listAnnotationsController = new ListAnnotationsController(annotationRepositoryMySql)
-const modifyAnnotationController = new ModifyAnnotationController(annotationRepositoryMySql)
-const removeAnnotationController = new RemoveAnnotationController(annotationRepositoryMySql)
+
+const createAnnotationService = new CreateAnnotationService(annotationRepositoryMySql)
+const listAnnotationService = new ListAnnotationService(annotationRepositoryMySql)
+const listAnnotationsService = new ListAnnotationsService(annotationRepositoryMySql)
+const modifyAnnotationService = new ModifyAnnotationService(annotationRepositoryMySql)
+const removeAnnotationService = new RemoveAnnotationService(annotationRepositoryMySql)
+
+const createAnnotationController = new CreateAnnotationController(createAnnotationService)
+const listAnnotationController = new ListAnnotationController(listAnnotationService)
+const listAnnotationsController = new ListAnnotationsController(listAnnotationsService)
+const modifyAnnotationController = new ModifyAnnotationController(modifyAnnotationService)
+const removeAnnotationController = new RemoveAnnotationController(removeAnnotationService)
 
 module.exports = (app) => {
   app.route('/annotations')
--- src/routes/List.js
+++ src/routes/List.js
@@ -1,18 +1,34 @@
-const authenticationToken = require('../middlewares/authenticationToken')
-const CreateListController = require('../components/List/CreateList/CreateListController')
 const { Lists } = require('../database/models')
+
+const authenticationToken = require('../middlewares/authenticationToken')
+
 const ListRepositoryMySql = require('../components/List/repositories/ListRepositoryMySql')
+
+const CreateListController = require('../components/List/CreateList/CreateListController')
 const ModifyListController = require('../components/List/ModifyList/ModifyListController')
 const RemoveListController = require('../components/List/RemoveList/RemoveListController')
 const ReturnListController = require('../components/List/ReturnList/ReturnListController')
 const ReturnListsController = require('../components/List/ReturnLists/ReturnListsController')
 
+const CreateListService = require('../components/List/CreateList/CreateListService')
+const ModifyListService = require('../components/List/ModifyList/ModifyListService')
+const RemoveListService = require('../components/List/RemoveList/RemoveListService')
+const ReturnListService = require('../components/List/ReturnList/ReturnListService')
+const ReturnListsService = require('../components/List/ReturnLists/ReturnListsService')
+
 const listRepositoryMySql = new ListRepositoryMySql(Lists)
-const createListController = new CreateListController(listRepositoryMySql)
-const listListController = new ReturnListController(listRepositoryMySql)
-const listListsController = new ReturnListsController(listRepositoryMySql)
-const modifyListController = new ModifyListController(listRepositoryMySql)
-const removeListController = new RemoveListController(listRepositoryMySql)
+
+const createListService = new CreateListService(listRepositoryMySql)
+const listListService = new ReturnListService(listRepositoryMySql)
+const listListsService = new ReturnListsService(listRepositoryMySql)
+const modifyListService = new ModifyListService(listRepositoryMySql)
+const removeListService = new RemoveListService(listRepositoryMySql)
+
+const createListController = new CreateListController(createListService)
+const listListController = new ReturnListController(listListService)
+const listListsController = new ReturnListsController(listListsService)
+const modifyListController = new ModifyListController(modifyListService)
+const removeListController = new RemoveListController(removeListService)
 
 module.exports = (app) => {
   app.route('/lists')
