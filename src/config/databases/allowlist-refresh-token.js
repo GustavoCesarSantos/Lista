001ace45c9590fb9744b7802c6c9e4bd70d2e8d6
Gustavo Cesar <gustavocs789@gmail.com>
2021-12-04 16:00:18 -0300

    wip

--- src/config/databases/allowlist-refresh-token.js
+++ src/config/databases/allowlist-refresh-token.js
@@ -2,7 +2,7 @@ const redis = require('redis')
 
 const genericAuthenticationListHelper = require('../../helpers/genericAuthenticationList')
 
-const url = process.env.REDISCLOUD_URL
+const url = process.env.NODE_ENV === 'production' ? process.env.REDISCLOUD_URL : process.env.DEV_CACHE_URL
 
 const allowlistRefreshToken = redis.createClient({
   url: url,
