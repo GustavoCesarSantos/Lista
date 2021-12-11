388045118304f0f162944ca11e86d511536084dc
Gustavo Cesar <gustavocs789@gmail.com>
2021-12-11 17:30:35 -0300

    feat(list): Created list factory using mysql

---
+++ src/components/List/factories/ListServiceWithMySqlFactory.js
@@ -0,0 +1,34 @@
+const CreateListService = require('../CreateList/CreateListService')
+const IListServiceFactory = require('./IListServiceFactory')
+const { List } = require('../../../database/models')
+const ListRepositoryMySql = require('../repositories/ListRepositoryMySql')
+const ModifyListService = require('../ModifyList/ModifyListService')
+const RemoveListService = require('../RemoveList/RemoveListService')
+const ReturnListService = require('../ReturnList/ReturnListService')
+const ReturnListsService = require('../ReturnLists/ReturnListsService')
+
+const listRepositoryMySql = new ListRepositoryMySql(List)
+
+class ListServiceWithMySqlFactory extends IListServiceFactory {
+  createListService () {
+    return new CreateListService(listRepositoryMySql)
+  }
+
+  returnListService () {
+    return new ReturnListService(listRepositoryMySql)
+  }
+
+  returnListsService () {
+    return new ReturnListsService(listRepositoryMySql)
+  }
+
+  modifyListService () {
+    return new ModifyListService(listRepositoryMySql)
+  }
+
+  removeListService () {
+    return new RemoveListService(listRepositoryMySql)
+  }
+}
+
+module.exports = ListServiceWithMySqlFactory
