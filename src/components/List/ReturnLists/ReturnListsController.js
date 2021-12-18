d8d955af99f1cb47ed7d7a023df3c4733d9cfdb6
Gustavo Cesar <gustavocs789@gmail.com>
2021-12-18 20:49:15 -0300

    fix(list): Refactored return lists controller layer

--- src/components/List/ReturnLists/ReturnListsController.js
+++ src/components/List/ReturnLists/ReturnListsController.js
@@ -1,10 +1,9 @@
 const logger = require('../../../helpers/logger');
-const ReturnListsService = require('./ReturnListsService');
 const ReturnListsRequestDTO = require('./ReturnListsRequestDTO');
 
 class ReturnListsController {
-	constructor(returnListsRepository) {
-		this.returnListsRepository = returnListsRepository;
+	constructor(returnListsService) {
+		this.returnListsService = returnListsService;
 	}
 
 	async handler(request, response) {
@@ -15,20 +14,19 @@ class ReturnListsController {
 			const returnListsRequestDTO = new ReturnListsRequestDTO({
 				...request.query,
 			});
-			const returnListsService = new ReturnListsService(
-				this.returnListsRepository,
-			);
-			const lists = await returnListsService.execute(
+			const lists = await this.returnListsService.execute(
 				returnListsRequestDTO,
 			);
 			logger.info(
 				`Usuário:${request.user.id} conseguiu retornar todas as listas.`,
 			);
-			response.status(200).json(lists);
+			response.status(200);
+			response.json(lists);
 		} catch (error) {
 			if (!error.httpCode) error.httpCode = 500;
 			logger.error(`${error.httpCode} - ${error.message}`);
-			response.status(error.httpCode).send(error.message);
+			response.status(error.httpCode);
+			response.send(error.message);
 		}
 	}
 }
