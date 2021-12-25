7adcfcdff3c9e396b2b8d8bec187b6d461f12a07
Gustavo Cesar <gustavocs789@gmail.com>
2021-12-25 19:16:48 -0300

    refactor: Refactored create chart controller

--- src/components/Annotation/CreateAnnotation/CreateAnnotationController.js
+++ src/components/Annotation/CreateAnnotation/CreateAnnotationController.js
@@ -1,33 +1,61 @@
 const CreateAnnotationRequestDTO = require('./CreateAnnotationRequestDTO');
-const logger = require('../../../helpers/logger');
+const HttpResponse = require('../../../helpers/HttpResponse');
+const InvalidParamError = require('../../../helpers/errors/InvalidParamError');
+// const logger = require('../../../helpers/logger');
+const MissingParamError = require('../../../helpers/errors/MissingParamError');
 
 class CreateAnnotationController {
-	constructor(createAnnotationService) {
+	constructor(createAnnotationService, paramTypeValidator) {
 		this.createAnnotationService = createAnnotationService;
+		this.paramTypeValidator = paramTypeValidator;
 	}
 
-	async handler(request, response) {
+	async handle(httpRequest) {
 		try {
-			logger.info(
-				`Usuário:${request.user.id} está tentando cadastrar uma anotação.`,
-			);
+			// logger.info(
+			// 	`Usuário:${request.user.id} está tentando cadastrar uma anotação.`,
+			// );
+			const { listId } = httpRequest.params;
+			const { contents } = httpRequest.body;
+			if (!listId) {
+				return HttpResponse.badRequest(
+					new MissingParamError('list id'),
+				);
+			}
+			if (!contents) {
+				return HttpResponse.badRequest(
+					new MissingParamError('contents'),
+				);
+			}
+			if (!this.paramTypeValidator.isString(listId)) {
+				return HttpResponse.badRequest(
+					new InvalidParamError('list id'),
+				);
+			}
+			if (!this.paramTypeValidator.isString(contents)) {
+				return HttpResponse.badRequest(
+					new InvalidParamError('contents'),
+				);
+			}
 			const createAnnotationRequestDTO = new CreateAnnotationRequestDTO({
-				...request.params,
-				...request.body,
+				...httpRequest.params,
+				...httpRequest.body,
 			});
 			await this.createAnnotationService.execute(
 				createAnnotationRequestDTO,
 			);
-			logger.info(
-				`Usuário:${request.user.id} conseguiu cadastrar a anotação.`,
-			);
-			response.status(201);
-			response.end();
-		} catch (err) {
-			if (!err.httpCode) err.httpCode = 500;
-			logger.error(`${err.httpCode} - ${err.message}`);
-			response.status(err.httpCode);
-			response.send(err.message);
+			// logger.info(
+			// 	`Usuário:${request.user.id} conseguiu cadastrar a anotação.`,
+			// );
+			// response.status(201);
+			// response.end();
+			return HttpResponse.okWithoutBody();
+		} catch (error) {
+			// logger.error(`${error.message}`);
+			return HttpResponse.serverError();
+			// if (!err.httpCode) err.httpCode = 500;
+			// response.status(err.httpCode);
+			// response.send(err.message);
 		}
 	}
 }
