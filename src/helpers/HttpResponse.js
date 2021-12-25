5c71f58c8033ed4d9e96f7607dc2ec97559efbcb
Gustavo Cesar <gustavocs789@gmail.com>
2021-12-25 19:13:32 -0300

    feat: Added success methods to http response wrapper

--- src/helpers/HttpResponse.js
+++ src/helpers/HttpResponse.js
@@ -1,6 +1,19 @@
-const ServerError = require('./Errors/ServerError');
+const ServerError = require('./errors/ServerError');
 
 class HttpResponse {
+	static ok(body) {
+		return {
+			statusCode: 200,
+			body,
+		};
+	}
+
+	static okWithoutBody() {
+		return {
+			statusCode: 201,
+		};
+	}
+
 	static badRequest(error) {
 		const statusCode = 400;
 		const message = error.message;
