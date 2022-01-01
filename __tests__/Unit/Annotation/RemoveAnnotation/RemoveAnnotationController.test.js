f849e38ff7e27ba13f6f27ba6ef41ca3b1009fd3
Gustavo Cesar <gustavocs789@gmail.com>
2022-01-01 00:23:04 -0300

    refactor: Refactored remove annotation controller test file

--- __tests__/Unit/Annotation/RemoveAnnotation/RemoveAnnotationController.test.js
+++ __tests__/Unit/Annotation/RemoveAnnotation/RemoveAnnotationController.test.js
@@ -1,98 +1,113 @@
+const InvalidParamError = require('../../../../src/helpers/Errors/InvalidParamError');
+const MissingParamError = require('../../../../src/helpers/Errors/MissingParamError');
 const RemoveAnnotationController = require('../../../../src/components/Annotation/RemoveAnnotation/RemoveAnnotationController');
-const logger = require('../../../../src/helpers/logger');
+const ServerError = require('../../../../src/helpers/Errors/ServerError');
+
+class RemoveAnnotationServiceDummy {
+	async execute() {}
+}
+
+class LoggerDummy {
+	info(message) {
+		return message;
+	}
+	error(message) {
+		return message;
+	}
+}
+
+class ParamTypeValidatorMock {
+	isString(param) {
+		return typeof param === 'string';
+	}
+}
+
+const makeSut = () => {
+	const removeAnnotationServiceDummy = new RemoveAnnotationServiceDummy();
+	const loggerDummy = new LoggerDummy();
+	const paramTypeValidatorMock = new ParamTypeValidatorMock();
+	const sut = new RemoveAnnotationController(
+		removeAnnotationServiceDummy,
+		loggerDummy,
+		paramTypeValidatorMock,
+	);
+	return sut;
+};
 
 describe('REMOVE ANNOTATION CONTROLLER UNIT TEST', () => {
-	beforeEach(() => {
-		jest.spyOn(logger, 'info').mockImplementation();
-
-		jest.spyOn(logger, 'error').mockImplementation();
+	test('Should return 500 if no http request is provided', async () => {
+		const sut = makeSut();
+		const httpResponse = await sut.handle();
+		expect(httpResponse.statusCode).toBe(500);
+		expect(httpResponse.message).toBe(new ServerError().message);
 	});
 
-	test('Should catch an error with default http status code', async () => {
-		const request = {
+	test('Should return 500 if http request has no params', async () => {
+		const httpRequest = {
 			user: { id: 1 },
-			params: { listId: 1 },
 		};
-		const response = {
-			status(statusCode) {
-				return statusCode;
-			},
-			send(message) {
-				return message;
-			},
-		};
-		const RemoveAnnotationServiceFake = jest
-			.fn()
-			.mockImplementation(() => ({
-				async execute(data) {
-					throw new Error('Teste');
-				},
-			}));
-		const spy = jest.spyOn(response, 'status');
-		const removeAnnotationServiceFake = new RemoveAnnotationServiceFake();
-		const removeAnnotationController = new RemoveAnnotationController(
-			removeAnnotationServiceFake,
-		);
-		await removeAnnotationController.handler(request, response);
-		expect(spy).toHaveBeenCalledTimes(1);
-		expect(spy).toHaveBeenCalledWith(500);
+		const sut = makeSut();
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(500);
+		expect(httpResponse.message).toBe(new ServerError().message);
 	});
 
-	test('Should catch an error with custom http status code', async () => {
-		const request = {
-			user: { id: 1 },
-			params: { listId: 1 },
+	test('Should return 500 if http request has no user', async () => {
+		const httpRequest = {
+			params: { annotationId: 1 },
 		};
-		const response = {
-			status(statusCode) {
-				return statusCode;
-			},
-			send(message) {
-				return message;
-			},
-		};
-		const RemoveAnnotationServiceFake = jest
-			.fn()
-			.mockImplementation(() => ({
-				async execute(data) {
-					const error = new Error('Teste');
-					error.httpCode = 401;
-					throw error;
-				},
-			}));
-		const spy = jest.spyOn(response, 'status');
-		const removeAnnotationServiceFake = new RemoveAnnotationServiceFake();
-		const removeAnnotationController = new RemoveAnnotationController(
-			removeAnnotationServiceFake,
-		);
-		await removeAnnotationController.handler(request, response);
-		expect(spy).toHaveBeenCalledTimes(1);
-		expect(spy).toHaveBeenCalledWith(401);
+		const sut = makeSut();
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(500);
+		expect(httpResponse.message).toBe(new ServerError().message);
 	});
 
-	test('Should pass', async () => {
-		const request = {
+	test('Should return 400 if no annotation id is provided', async () => {
+		const httpRequest = {
+			params: {},
 			user: { id: 1 },
-			params: { listId: 1 },
 		};
-		const response = {
-			status(statusCode) {
-				return statusCode;
-			},
-			end() {},
-		};
-		const spy = jest.spyOn(response, 'status');
-		const RemoveAnnotationServiceFake = jest
-			.fn()
-			.mockImplementation(() => ({
-				execute: data => {},
-			}));
-		const removeAnnotationServiceFake = new RemoveAnnotationServiceFake();
-		const removeAnnotationController = new RemoveAnnotationController(
-			removeAnnotationServiceFake,
+		const sut = makeSut();
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.message).toBe(
+			new MissingParamError('annotation id').message,
 		);
-		await removeAnnotationController.handler(request, response);
-		expect(spy).toHaveBeenCalledTimes(1);
-		expect(spy).toHaveBeenCalledWith(201);
+	});
+
+	test('Should return 400 if no user id is provided', async () => {
+		const httpRequest = {
+			params: { annotationId: 1 },
+			user: {},
+		};
+		const sut = makeSut();
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.message).toBe(
+			new MissingParamError('user id').message,
+		);
+	});
+
+	test('Should return 400 if an invalid annotation id is provided', async () => {
+		const httpRequest = {
+			params: { annotationId: 1 },
+			user: { id: 1 },
+		};
+		const sut = makeSut();
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.message).toBe(
+			new InvalidParamError('annotation id').message,
+		);
+	});
+
+	test('Should return 201 when valid infos are provided', async () => {
+		const httpRequest = {
+			params: { annotationId: '1' },
+			user: { id: 1 },
+		};
+		const sut = makeSut();
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(201);
 	});
 });
