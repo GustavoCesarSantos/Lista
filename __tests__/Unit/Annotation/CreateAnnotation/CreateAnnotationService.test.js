49e7082865b65dc2ac5b8aa84916bd7644ee015f
Gustavo Cesar <gustavocs789@gmail.com>
2022-01-01 00:17:01 -0300

    refactor: Refactored create annotation service test file

--- __tests__/Unit/Annotation/CreateAnnotation/CreateAnnotationService.test.js
+++ __tests__/Unit/Annotation/CreateAnnotation/CreateAnnotationService.test.js
@@ -1,21 +1,34 @@
 const CreateAnnotationService = require('../../../../src/components/Annotation/CreateAnnotation/CreateAnnotationService');
 
+class AnnotationRepositoryDummy {
+	create() {}
+}
+
+const makeSut = () => {
+	const annotationRepositoryDummy = new AnnotationRepositoryDummy();
+	const sut = new CreateAnnotationService(annotationRepositoryDummy);
+	return sut;
+};
+
 describe('CREATE ANNOTATION SERVICE UNIT TEST', () => {
-	test('Should create a valid annotation', async () => {
+	test('Should return 500 if no repository has no provided', async () => {
+		const sut = new CreateAnnotationService();
 		const annotation = {
 			id: 1,
 			listId: 1,
 			contents: 'teste',
 		};
-		const AnnotationRepositoryFake = jest.fn().mockImplementation(() => ({
-			async create(data) {},
-		}));
-		const annotationRepositoryFake = new AnnotationRepositoryFake();
-		const createAnnotationService = new CreateAnnotationService(
-			annotationRepositoryFake,
-		);
-		await expect(
-			createAnnotationService.execute(annotation),
-		).resolves.not.toThrow();
+		const httpResponse = sut.execute(annotation);
+		expect(httpResponse).rejects.toThrow();
+	});
+
+	test('Should create a valid annotation', async () => {
+		const sut = makeSut();
+		const annotation = {
+			id: 1,
+			listId: 1,
+			contents: 'teste',
+		};
+		expect(sut.execute(annotation)).resolves.not.toThrow();
 	});
 });
