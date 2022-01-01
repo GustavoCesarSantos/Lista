48c2efbf356458886f56196b2c44b25c6b10488d
Gustavo Cesar <gustavocs789@gmail.com>
2022-01-01 00:22:19 -0300

    refactor: Refactored modify annotation service test file

--- __tests__/Unit/Annotation/ModifyAnnotation/ModifyAnnotationService.test.js
+++ __tests__/Unit/Annotation/ModifyAnnotation/ModifyAnnotationService.test.js
@@ -1,38 +1,59 @@
 const ModifyAnnotationService = require('../../../../src/components/Annotation/ModifyAnnotation/ModifyAnnotationService');
+const NotFoundError = require('../../../../src/helpers/errors/NotFoundError');
+
+class AnnotationRepositoryFake {
+	findOne(annotationId) {
+		annotationId;
+		return true;
+	}
+
+	modify() {}
+}
+
+class AnnotationRepositoryWithErrorFake {
+	findOne(annotationId) {
+		annotationId;
+		return false;
+	}
+}
+
+const makeSut = () => {
+	const annotationRepositoryFake = new AnnotationRepositoryFake();
+	const sut = new ModifyAnnotationService(annotationRepositoryFake);
+	return sut;
+};
 
 describe('MODIFY ANNOTATION SERVICE UNIT TEST', () => {
-	test('Should throw  an error when not found an annotation', async () => {
-		const annotation = { id: 1 };
-		const AnnotationRepositoryFake = jest.fn().mockImplementation(() => ({
-			async findOne(data) {
-				return false;
-			},
-		}));
-		const annotationRepositoryFake = new AnnotationRepositoryFake();
-		const modifyAnnotationService = new ModifyAnnotationService(
-			annotationRepositoryFake,
-		);
-		await modifyAnnotationService
-			.execute(annotation)
-			.catch(error =>
-				expect(error.message).toBe('Anotação não encontrada'),
-			);
+	test('Should return 500 if no repository has no provided', async () => {
+		const sut = new ModifyAnnotationService();
+		const annotation = {
+			id: 1,
+			listId: 1,
+			contents: 'teste',
+		};
+		const httpResponse = sut.execute(annotation);
+		expect(httpResponse).rejects.toThrow();
 	});
 
-	test('Should return an annotation', async () => {
-		const annotation = { id: 1 };
-		const AnnotationRepositoryFake = jest.fn().mockImplementation(() => ({
-			async findOne(data) {
-				return true;
-			},
-			async modify(data) {},
-		}));
-		const annotationRepositoryFake = new AnnotationRepositoryFake();
-		const modifyAnnotationService = new ModifyAnnotationService(
-			annotationRepositoryFake,
+	test('Should return 404 if an annotation not founded', async () => {
+		const annotationRepositoryWithErrorFake =
+			new AnnotationRepositoryWithErrorFake();
+		const sut = new ModifyAnnotationService(
+			annotationRepositoryWithErrorFake,
 		);
-		await expect(
-			modifyAnnotationService.execute(annotation),
-		).resolves.not.toThrow();
+		const annotation = {
+			annotationId: 1,
+		};
+		expect(sut.execute(annotation)).rejects.toThrow(
+			new NotFoundError(`${annotation.annotationId}`),
+		);
+	});
+
+	test('Should return a valid annotation', async () => {
+		const sut = makeSut();
+		const annotation = {
+			annotationId: 1,
+		};
+		expect(sut.execute(annotation)).resolves.not.toThrow();
 	});
 });
