fa21b4f9a6461c8a9aac5b1da9bb91adaf0f0de3
Gustavo Cesar <gustavocs789@gmail.com>
2022-01-03 00:33:48 -0300

    fix: Added imports in constructor

--- src/routes/Annotation.js
+++ src/routes/Annotation.js
@@ -2,24 +2,35 @@ const authenticationToken = require('../middlewares/authenticationToken');
 const AnnotationServiceWithMySqlFactory = require('../components/Annotation/factories/AnnotationServiceWithMySqlFactory');
 const CreateAnnotationController = require('../components/Annotation/CreateAnnotation/CreateAnnotationController');
 const ModifyAnnotationController = require('../components/Annotation/ModifyAnnotation/ModifyAnnotationController');
+const ParamTypeValidator = require('../helpers/ParamTypeValidator');
 const RemoveAnnotationController = require('../components/Annotation/RemoveAnnotation/RemoveAnnotationController');
 const ReturnAnnotationController = require('../components/Annotation/ReturnAnnotation/ReturnAnnotationController');
 const ReturnAnnotationsController = require('../components/Annotation/ReturnAnnotations/ReturnAnnotationsController');
+const WinstonLog = require('../helpers/logs/WinstonLog');
 
 const createAnnotationController = new CreateAnnotationController(
 	AnnotationServiceWithMySqlFactory.createAnnotationService(),
+	WinstonLog,
+	ParamTypeValidator,
 );
 const returnAnnotationController = new ReturnAnnotationController(
 	AnnotationServiceWithMySqlFactory.returnAnnotationService(),
+	WinstonLog,
+	ParamTypeValidator,
 );
 const returnAnnotationsController = new ReturnAnnotationsController(
 	AnnotationServiceWithMySqlFactory.returnAnnotationsService(),
+	WinstonLog,
 );
 const modifyAnnotationController = new ModifyAnnotationController(
 	AnnotationServiceWithMySqlFactory.modifyAnnotationService(),
+	WinstonLog,
+	ParamTypeValidator,
 );
 const removeAnnotationController = new RemoveAnnotationController(
 	AnnotationServiceWithMySqlFactory.removeAnnotationService(),
+	WinstonLog,
+	ParamTypeValidator,
 );
 
 module.exports = app => {
