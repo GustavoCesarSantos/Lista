e155b93e9ddf273169bec60df0d6ef0445dc0506
Gustavo Cesar <gustavocs789@gmail.com>
2022-01-15 15:45:17 -0300

    feat: Created express route adapter

---
+++ src/infra/adapters/expressRoutesAdapter.js
@@ -0,0 +1,12 @@
+const adaptRoute = controller => {
+	return async (request, response) => {
+		const httpResponse = controller.handle(request);
+		if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+			response.status(httpResponse.statusCode).json(httpResponse.body);
+		} else {
+			response.status(httpResponse.statusCode).json(httpResponse.message);
+		}
+	};
+};
+
+module.exports = adaptRoute;
