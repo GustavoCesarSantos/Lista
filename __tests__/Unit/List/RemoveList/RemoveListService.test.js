ad10957af549c9c94ce22ab95ede39d350ea52de
Gustavo Cesar <gustavocs789@gmail.com>
2022-02-05 16:47:19 -0300

    refactor: Refactor remove list service unit test

--- __tests__/Unit/List/RemoveList/RemoveListService.test.js
+++ __tests__/Unit/List/RemoveList/RemoveListService.test.js
@@ -1,30 +1,56 @@
+const NotFoundError = require('../../../../src/helpers/errors/NotFoundError');
 const RemoveListService = require('../../../../src/components/List/RemoveList/RemoveListService');
 
+class ListRepositoryFake {
+	findOne(listId) {
+		listId;
+		return true;
+	}
+
+	remove() {}
+}
+
+class ListRepositoryWithErrorFake {
+	findOne(listId) {
+		listId;
+		return false;
+	}
+
+	remove() {}
+}
+
+const makeSut = () => {
+	const listRepositoryFake = new ListRepositoryFake();
+	const sut = new RemoveListService(listRepositoryFake);
+	return sut;
+};
+
 describe('REMOVE LIST SERVICE UNIT TEST', () => {
-	test('Should throw  an error when not found an list', async () => {
-		const list = { id: 1 };
-		const ListRepositoryFake = jest.fn().mockImplementation(() => ({
-			async findOne(data) {
-				return false;
-			},
-		}));
-		const listRepositoryFake = new ListRepositoryFake();
-		const removeListService = new RemoveListService(listRepositoryFake);
-		await removeListService
-			.execute(list)
-			.catch(error => expect(error.message).toBe('Lista não encontrada'));
+	test('Should return 500 if no repository has no provided', async () => {
+		const sut = new RemoveListService();
+		const list = {
+			listId: 1,
+		};
+		const httpResponse = sut.execute(list);
+		expect(httpResponse).rejects.toThrow();
 	});
 
-	test('Should remove a list', async () => {
-		const list = { id: 1 };
-		const ListRepositoryFake = jest.fn().mockImplementation(() => ({
-			async findOne(data) {
-				return true;
-			},
-			async remove(data) {},
-		}));
-		const listRepositoryFake = new ListRepositoryFake();
-		const removeListService = new RemoveListService(listRepositoryFake);
-		await expect(removeListService.execute(list)).resolves.not.toThrow();
+	test('Should return 404 if an list not founded', async () => {
+		const listRepositoryWithErrorFake = new ListRepositoryWithErrorFake();
+		const sut = new RemoveListService(listRepositoryWithErrorFake);
+		const list = {
+			listId: 1,
+		};
+		expect(sut.execute(list)).rejects.toThrow(
+			new NotFoundError(`${list.listId}`),
+		);
+	});
+
+	test('Should remove an list', async () => {
+		const sut = makeSut();
+		const list = {
+			listId: 1,
+		};
+		expect(sut.execute(list)).resolves.not.toThrow();
 	});
 });
