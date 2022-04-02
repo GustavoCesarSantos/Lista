4cf5e5f5989dbee8ff4ac5ca170d93b7a0d74ad0
Gustavo Cesar <gustavocs789@gmail.com>
2022-04-02 15:51:53 -0300

    feat: Adiciona parametros aos construtores das classes fabricas de anotação e lista

--- src/components/Annotation/factories/AnnotationControllerFactory.js
+++ src/components/Annotation/factories/AnnotationControllerFactory.js
@@ -4,35 +4,46 @@ const ModifyAnnotationController = require('../ModifyAnnotation/ModifyAnnotation
 const RemoveAnnotationController = require('../RemoveAnnotation/RemoveAnnotationController');
 const ReturnAnnotationController = require('../ReturnAnnotation/ReturnAnnotationController');
 const ReturnAnnotationsController = require('../ReturnAnnotations/ReturnAnnotationsController');
+const WinstonLog = require('../../../helpers/logs/WinstonLog');
+const ParamTypeValidator = require('../../../helpers/ParamTypeValidator');
 
 class AnnotationControllerFactory {
 	static makeCreateAnnotationController() {
 		return new CreateAnnotationController(
 			AnnotationServiceWithMySqlFactory.createAnnotationService(),
+			WinstonLog,
+			ParamTypeValidator,
 		);
 	}
 
 	static makeReturnAnnotationController() {
 		return new ReturnAnnotationController(
 			AnnotationServiceWithMySqlFactory.returnAnnotationService(),
+			WinstonLog,
+			ParamTypeValidator,
 		);
 	}
 
 	static makeReturnAnnotationsController() {
 		return new ReturnAnnotationsController(
 			AnnotationServiceWithMySqlFactory.returnAnnotationsService(),
+			WinstonLog,
 		);
 	}
 
 	static makeModifyAnnotationController() {
 		return new ModifyAnnotationController(
 			AnnotationServiceWithMySqlFactory.modifyAnnotationService(),
+			WinstonLog,
+			ParamTypeValidator,
 		);
 	}
 
 	static makeRemoveAnnotationController() {
 		return new RemoveAnnotationController(
 			AnnotationServiceWithMySqlFactory.removeAnnotationService(),
+			WinstonLog,
+			ParamTypeValidator,
 		);
 	}
 }
